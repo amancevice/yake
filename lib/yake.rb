db38dd8f7c28808783be3390ef9d135781a417a8
Alexander <alexander.mancevice@hey.com>
2024-01-01 11:18:16 -0500

    quotes

--- lib/yake.rb
+++ lib/yake.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative "yake/version"
-require_relative "yake/logger"
-require_relative "yake/errors"
-require_relative "yake/dsl"
+require_relative 'yake/version'
+require_relative 'yake/logger'
+require_relative 'yake/errors'
+require_relative 'yake/dsl'
